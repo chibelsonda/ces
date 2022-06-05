2be6979f5b67ddd15bf6a40dce6f9736c1323588
Chicote Belsonda <belsondachi@gmail.com>
2022-06-05 13:05:38 +0800

    Fixed subject pages layout

--- resources/views/subject/create.blade.php
+++ resources/views/subject/create.blade.php
@@ -10,52 +10,55 @@
             </ol>
         </div>
         
-        <div class="col-12">
-            <div class="float-end mb-3">
+        <div class="col-12 col-lg-6">
+            <div class="float-end">
                 <a class="btn btn-sm btn-success" href="{{ route('subjects.index') }}"> Back</a>
             </div>
         </div>
     </div>
     
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    
-    <form action="{{ route('subjects.store') }}" method="POST">
-        @csrf
-    
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Code:</strong>
-                    <input type="text" name="code" class="form-control" placeholder="Code" required>
+    <div class="row">
+        <div class="col-12 col-lg-6">
+            @if ($errors->any())
+                <div class="alert alert-danger mt-2">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
                 </div>
-            </div>
+            @endif
+            
+            <form action="{{ route('subjects.store') }}" method="POST">
+                @csrf
+            
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <label>Code:</label>
+                            <input type="text" name="code" class="form-control" placeholder="Code" required>
+                        </div>
+                    </div>
 
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Description:</strong>
-                    <textarea class="form-control" style="height:150px" name="description" placeholder="Description" required></textarea>
-                </div>
-            </div>
+                    <div class="col-12 col-sm-12 col-md-12">
+                        <div class="form-group mt-2">
+                            <label>Description:</label>
+                            <textarea class="form-control" style="height:150px" name="description" placeholder="Description" required></textarea>
+                        </div>
+                    </div>
 
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Units:</strong>
-                    <input type="text" name="units" class="form-control" placeholder="Units" required>
+                    <div class="col-12 col-sm-12 col-md-12">
+                        <div class="form-group mt-2">
+                            <label>Units:</label>
+                            <input type="text" name="units" class="form-control" placeholder="Units" required>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-12 text-center mt-3">
+                            <button type="submit" class="btn btn-primary w-100">Submit</button>
+                    </div>
                 </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
+            
+            </form>
         </div>
-    
-    </form>
+    </div>
 @endsection
--- resources/views/subject/edit.blade.php
+++ resources/views/subject/edit.blade.php
@@ -9,57 +9,61 @@
             </ol>
         </div>
         
-        <div class="col-12">
-            <div class="float-end mb-3">
+        <div class="col-12 col-lg-6">
+            <div class="float-end">
                 <a class="btn btn-sm btn-success" href="{{ route('subjects.index') }}"> Back</a>
             </div>
         </div>
     </div>
     
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+    <div class="row">
+        <div class="col-12 col-lg-6">
+
+            @if ($errors->any())
+                <div class="alert alert-danger mt-2">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            
+            <form action="{{ route('subjects.update', $subject) }}" method="POST">
+                @csrf
+                @method('PUT')
+            
+                <div class="row">
+                    <input type="hidden" name="id" value="{{ $subject->id }}">
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <label>Code:</label>
+                            <input type="text" name="code" class="form-control" value="{{  $subject->code }}" placeholder="Code" required>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group mt-2">
+                            <label>Description:</label>
+                            <textarea class="form-control" style="height:150px" name="description" placeholder="Description" required>
+                                {{  $subject->description }}
+                            </textarea>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group mt-2">
+                            <label>Units:</label>
+                            <input type="text" name="units" class="form-control" value="{{  $subject->units }}"  placeholder="Units" required>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 text-center mt-3">
+                        <button type="submit" class="btn btn-primary w-100">Submit</button>
+                    </div>
+                </div>
+            
+            </form>
         </div>
-    @endif
-    
-    <form action="{{ route('subjects.update', $subject) }}" method="POST">
-        @csrf
-        @method('PUT')
-    
-        <div class="row">
-            <input type="hidden" name="id" value="{{ $subject->id }}">
-
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Code:</strong>
-                    <input type="text" name="code" class="form-control" value="{{  $subject->code }}" placeholder="Code" required>
-                </div>
-            </div>
-
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Description:</strong>
-                    <textarea class="form-control" style="height:150px" name="description" placeholder="Description" required>
-                        {{  $subject->description }}
-                    </textarea>
-                </div>
-            </div>
-
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Units:</strong>
-                    <input type="text" name="units" class="form-control" value="{{  $subject->units }}"  placeholder="Units" required>
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-        </div>
-    
-    </form>
+    </div>
 @endsection
