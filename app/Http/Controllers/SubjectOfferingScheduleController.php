eea5c5087f51a2b453b53c339d5e36fae43b91ba
Chicote Belsonda <belsondachi@gmail.com>
2022-06-06 21:52:24 +0800

    Added subject offering schedule files

---
+++ app/Http/Controllers/SubjectOfferingScheduleController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSubjectOfferingScheduleRequest;
+use App\Http\Requests\UpdateSubjectOfferingScheduleRequest;
+use App\Models\SubjectOfferingSchedule;
+
+class SubjectOfferingScheduleController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(StoreSubjectOfferingScheduleRequest $request)
+    {
+        //
+    }
+
+    public function show(SubjectOfferingSchedule $subjectOfferingSchedule)
+    {
+        //
+    }
+
+    public function edit(SubjectOfferingSchedule $subjectOfferingSchedule)
+    {
+        //
+    }
+
+    public function update(UpdateSubjectOfferingScheduleRequest $request, SubjectOfferingSchedule $subjectOfferingSchedule)
+    {
+        //
+    }
+
+    public function destroy(SubjectOfferingSchedule $subjectOfferingSchedule)
+    {
+        //
+    }
+}
