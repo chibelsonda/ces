15967be176909bd6bf748af0f02e4a90c4fc7820
Chicote Belsonda <belsondachi@gmail.com>
2022-06-06 21:47:59 +0800

    Added instructor and room files

---
+++ app/Http/Controllers/InstructorController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreInstructorRequest;
+use App\Http\Requests\UpdateInstructorRequest;
+use App\Models\Instructor;
+
+class InstructorController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(StoreInstructorRequest $request)
+    {
+        //
+    }
+
+    public function show(Instructor $instructor)
+    {
+        //
+    }
+
+    public function edit(Instructor $instructor)
+    {
+        //
+    }
+
+    public function update(UpdateInstructorRequest $request, Instructor $instructor)
+    {
+        //
+    }
+
+    public function destroy(Instructor $instructor)
+    {
+        //
+    }
+}
