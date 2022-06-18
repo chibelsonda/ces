3cf60749ba3e985ac490e3214b2e5185307c1bac
Chicote Belsonda <belsondachi@gmail.com>
2022-06-18 14:17:59 +0800

    Updated schedule migration

--- database/migrations/2022_06_06_134852_create_subject_offering_schedules_table.php
+++ database/migrations/2022_06_06_134852_create_subject_offering_schedules_table.php
@@ -4,28 +4,34 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-// return new class extends Migration
-// {
-//     /**
-//      * Run the migrations.
-//      *
-//      * @return void
-//      */
-//     public function up()
-//     {
-//         Schema::create('subject_offering_schedules', function (Blueprint $table) {
-//             $table->id();
-//             $table->timestamps();
-//         });
-//     }
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subject_offering_schedules', function (Blueprint $table) {
+            $table->id();
+            $table->integer('subject_offering_id')->unsigned();
+            $table->integer('instructor_id')->unsigned();
+            $table->integer('room_id')->unsigned();
+            $table->string('days', 50);
+            $table->time('time_start');
+            $table->time('time_end');
+            $table->timestamps();
+        });
+    }
 
-//     /**
-//      * Reverse the migrations.
-//      *
-//      * @return void
-//      */
-//     public function down()
-//     {
-//         Schema::dropIfExists('subject_offering_schedules');
-//     }
-// };
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subject_offering_schedules');
+    }
+};
